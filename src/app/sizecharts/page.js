541010cfc27f8c6fe4f5d058c99eb258e883f919
aboiko1281 <aboiko1281@gmail.com>
2024-02-19 14:11:48 +0000

    routing menu AppBar.js and add page

---
+++ src/app/sizecharts/page.js
@@ -0,0 +1,27 @@
+'use client'
+import Link from '@mui/material/Link';
+import * as NextLink from 'next/link';
+import DrawerAppBar from "/src/components/AppBar";
+import * as React from 'react';
+import Box from "@mui/material/Box";
+export default function SizeCharts() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            {/*<header>*/}
+            {/*<drawerAppBar/>*/}
+            <h1>SizeCharts</h1>
+            <DrawerAppBar/>
+            {/*</header>*/}
+            <Box sx={{display: {xs: 'none', sm: 'block'}}}>
+
+                <Link component={NextLink}
+                      sx={{color: '#fff'}}
+                      href="/"
+                >
+                    To Home page
+                </Link>
+
+            </Box>
+        </div>
+    );
+}
